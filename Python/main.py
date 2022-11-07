764cc81ef7fbf24d6cb0a77496648683d99be7cf
Martras2 <99558930+Martras2@users.noreply.github.com>
2022-11-06 19:02:24 -0600

    bug fix maybe

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,7 @@
 PATH = "C:/Users/Tomrio/Documents/Paradox Interactive/Crusader Kings III/logs/debug.log"
+from ahk import AHK
+from ahk.window import Window
+ahk = AHK()
 import pygame
 import time
 import re
@@ -10,10 +13,12 @@ import win32gui
 import autoit
 import accessible_output2.outputs.auto
 
-window_id = win32gui.FindWindow("SDL_app", "Crusader Kings III")
-#print(window_id)
+window_id = win32gui.FindWindow("SDL_app", "Crusader Kings")
+win = ahk.find_window(title=b'Crusader Kings III') # Find the opened window#
+#win = Window.from_pid(ahk, pid=window_id)                 # by process IDprint(window_id)
+print(win.title)
 #autoit.mouse_move(50,50,0)
-#autoit.control_send_by_handle(window_id, window_id, "{F1}")
+#win.send("{F1}")
 
 ao_output = accessible_output2.outputs.auto.Auto()
 
@@ -29,7 +34,7 @@ hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 # Set window transparency color
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 1, win32con.LWA_ALPHA )
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 100, win32con.LWA_ALPHA )
 
 screen.fill(fuchsia)  # Transparent background
 pygame.display.update()
@@ -74,140 +79,141 @@ while True:
    for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
          if event.key == pygame.K_F1 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F1}")
+            win.send("{F1}")
          elif event.key == pygame.K_F2 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F2}")
+            win.send("{F2}")
          elif event.key == pygame.K_F3 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F3}")
+            win.send("{F3}")
          elif event.key == pygame.K_F4 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F4}")
+            win.send("{F4}")
          elif event.key == pygame.K_F5 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F5}")
+            win.send("{F5}")
          elif event.key == pygame.K_F6 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F6}")
+            win.send("{F6}")
          elif event.key == pygame.K_F7 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F7}")
+            win.send("{F7}")
          elif event.key == pygame.K_F8 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F8}")
+            win.send("{F8}")
          elif event.key == pygame.K_F9 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F9}")
+            win.send("{F9}")
          elif event.key == pygame.K_c and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "c")
+            win.send("c")
          elif event.key == pygame.K_v and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "v")
+            win.send("v")
          elif event.key == pygame.K_BACKQUOTE and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "`")
+            win.send("`")
          elif event.key == pygame.K_ESCAPE and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{ESC}")
+            win.send("{ESC}")
          elif event.key == pygame.K_b and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "b")
+            win.send("b")
          elif event.key == pygame.K_F9 and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}{F9}{CTRLUP}")
+            win.send("{CTRLDOWN}{F9}{CTRLUP}")
 
          elif event.key == pygame.K_e and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "e")
+            win.send("e")
          elif event.key == pygame.K_a and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}a{CTRLUP}")
+            win.send("{CTRLDOWN}a{CTRLUP}")
          elif event.key == pygame.K_a and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}a{CTRLUP}")
+            win.send("{CTRLDOWN}a{CTRLUP}")
 
          elif (event.key == pygame.K_u and pygame.key.get_mods() == 0) or (event.key == pygame.K_q and pygame.key.get_mods() & pygame.KMOD_SHIFT):
-            autoit.control_send_by_handle(window_id, window_id, "u")
+            win.send("u")
          elif (event.key == pygame.K_i and pygame.key.get_mods() == 0) or (event.key == pygame.K_w and pygame.key.get_mods() & pygame.KMOD_SHIFT):
-            autoit.control_send_by_handle(window_id, window_id, "i")
+            win.send("i")
          elif (event.key == pygame.K_o and pygame.key.get_mods() == 0) or (event.key == pygame.K_e and pygame.key.get_mods() & pygame.KMOD_SHIFT):
-            autoit.control_send_by_handle(window_id, window_id, "o")
+            win.send("o")
          elif event.key == pygame.K_r and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "r")
+            win.send("r")
          elif event.key == pygame.K_t and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "t")
+            win.send("t")
          elif event.key == pygame.K_y and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "y")
+            win.send("y")
          elif event.key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}s{CTRLUP}")
+            win.send("{CTRLDOWN}s{CTRLUP}")
          elif event.key == pygame.K_q and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}q{CTRLUP}")
+            win.send("{CTRLDOWN}q{CTRLUP}")
          elif event.key == pygame.K_e and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}e{CTRLUP}")
+            win.send("{CTRLDOWN}e{CTRLUP}")
          elif event.key == pygame.K_w and pygame.key.get_mods() & pygame.KMOD_CTRL:
-            autoit.control_send_by_handle(window_id, window_id, "{CTRLDOWN}w{CTRLUP}")
+            win.send("{CTRLDOWN}w{CTRLUP}")
 #Map movement controls
          elif event.key == pygame.K_UP and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{UP}")
+            win.send("{UP}")
          elif event.key == pygame.K_DOWN and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{DOWN}")
+            win.send('hhf')  # Send keys, as if typed (performs ahk string escapes)
+
+#            win.send("{DOWN}")
          elif event.key == pygame.K_LEFT and pygame.key.get_mods() == 0 :
-#            autoit.control_send_by_handle(window_id, window_id, "{LEFT}")
-            autoit.control_send_by_handle(window_id, window_id, "h")
+#            win.send("{LEFT}")
+            win.send("h")
             time.sleep(.1)
-            autoit.control_send_by_handle(window_id, window_id, "f")
+            win.send("f")
             time.sleep(.1)
-            autoit.control_send_by_handle(window_id, window_id, "hgggghhhhghhhg")
+            win.send("hgggghhhhghhhg")
             time.sleep(.1)
-            autoit.control_send_by_handle(window_id, window_id, "f")
+            win.send("f")
 
          elif event.key == pygame.K_RIGHT and pygame.key.get_mods() == 0 :
-#            autoit.control_send_by_handle(window_id, window_id, "hgggghgghgghgg")
-            autoit.control_send_by_handle(window_id, window_id, "hg")
+#            win.send("hgggghgghgghgg")
+            win.send("hg")
             time.sleep(.1)
-            autoit.control_send_by_handle(window_id, window_id, "f")
+            win.send("f")
             time.sleep(.1)
 
-            autoit.control_send_by_handle(window_id, window_id, "hgggghgghgghgg")
+            win.send("hgggghgghgghgg")
             time.sleep(.1)
-            autoit.control_send_by_handle(window_id, window_id, "f")
+            win.send("f")
 
-#            autoit.control_send_by_handle(window_id, window_id, "{RIGHT}")
+#            win.send("{RIGHT}")
          elif event.key == pygame.K_HOME and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{HOME}")
+            win.send("{HOME}")
          elif event.key == pygame.K_PAGEUP and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{PGUP}")
+            win.send("{PGUP}")
          elif event.key == pygame.K_PAGEDOWN and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{PGDN}")
+            win.send("{PGDN}")
          elif event.key == pygame.K_f and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "f")
+            win.send("f")
          elif event.key == pygame.K_g and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "g")
+            win.send("g")
          elif event.key == pygame.K_h and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "h")
+            win.send("h")
          elif event.key == pygame.K_j and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "j")
+            win.send("j")
          elif event.key == pygame.K_1 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "1")
+            win.send("1")
          elif event.key == pygame.K_2 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "2")
+            win.send("2")
          elif event.key == pygame.K_3 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "3")
+            win.send("3")
          elif event.key == pygame.K_4 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "4")
+            win.send("4")
          elif event.key == pygame.K_5 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "5")
+            win.send("5")
          elif event.key == pygame.K_z and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "z")
+            win.send("z")
          elif event.key == pygame.K_x and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "x")
+            win.send("x")
          elif event.key == pygame.K_MINUS and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "-")
+            win.send("-")
          elif event.key == pygame.K_KP_MINUS and (pygame.key.get_mods()  == 0 or (pygame.key.get_mods() & pygame.KMOD_NUM	)):
-            autoit.control_send_by_handle(window_id, window_id, "{NUMPADSUB}")
+            win.send("{NUMPADSUB}")
          elif event.key == pygame.K_KP_PLUS and (pygame.key.get_mods()  == 0 or (pygame.key.get_mods() & pygame.KMOD_NUM	)):
-            autoit.control_send_by_handle(window_id, window_id, "{NUMPADADD}")
+            win.send("{NUMPADADD}")
          elif event.key == pygame.K_SPACE and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{SPACE}")
+            win.send("{SPACE}")
          elif event.key == pygame.K_F11 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F11}")
+            win.send("{F11}")
          elif event.key == pygame.K_F11 and pygame.key.get_mods() & pygame.KMOD_SHIFT:
-            autoit.control_send_by_handle(window_id, window_id, "{SHIFTDOWN}{F11}{SHIFTUP}")
+            win.send("{SHIFTDOWN}{F11}{SHIFTUP}")
          elif event.key == pygame.K_F12 and pygame.key.get_mods() == 0 :
-            autoit.control_send_by_handle(window_id, window_id, "{F12}")
+            win.send("{F12}")
 
       elif event.type == pygame.MOUSEBUTTONDOWN :
          if event.button == 1:
-            autoit.control_click_by_handle(window_id, window_id, button = 'left')
+#            autoit.control_click_by_handle(window_id, window_id, button = 'left')
          elif event.button == 3:
-            autoit.control_click_by_handle(window_id, window_id, button = 'right')
+#            autoit.control_click_by_handle(window_id, window_id, button = 'right')
             
       elif event.type == pygame.QUIT:
          pygame.quit()
          sys.exit()
-         
